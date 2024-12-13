ec37d50fb37dc0197bd954584387e99196966836
Marllon Home <m.w.p.ferreira@outlook.com>
2024-12-13 10:53:53 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
@@ -8,7 +8,7 @@ import { ProjetosComponent } from "./projetos/projetos.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomeComponent, AboutComponent, ProjetosComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, AboutComponent, ProjetosComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { provideRouter, withRouterConfig } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ProjetosComponent } from './projetos/projetos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjetosComponent },
+];
